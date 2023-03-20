04d5719e4768f85889cb0e78b322e8850dd32c05
Bdempsey03 <bdempsey@unbc.ca>
2023-03-20 14:33:30 -0700

    chagned font

--- src/Visuals/ControlPanel.java
+++ src/Visuals/ControlPanel.java
@@ -18,6 +18,11 @@ public class ControlPanel extends JComponent {
     private BasicStroke stroke = new BasicStroke((float)(X/100.));
     private BasicStroke stroke2 = new BasicStroke((float)(X/1000.));
 
+    Font font;
+
+    GraphicsEnvironment ge;
+
+
     private BufferedImage battleship;
     private BufferedImage aircraftcarrier;
     private BufferedImage submarine;
@@ -44,16 +49,33 @@ public class ControlPanel extends JComponent {
         g2d.fillRect(0, 0, X, Y);
         g2d.setColor(Color.BLACK);
 
+//        g2d.scale(2/3., 2/3.);
+        g2d.setFont(font);
+        g2d.setColor(Color.RED);
+//        g2d.scale(X/800., 1);
+        g2d.drawString("Opponent Remaining Ships:", 5*Y/7, 5*Y/14);
+//        g2d.scale(1,1);
+        g2d.setColor(Color.BLACK);
+        g2d.fillRect(3*X/8 - (X/50), 3*Y/7 - (Y/30), 5*X/8, 4*Y/7 );
+        g2d.setColor(Color.white);
+        for(int i =0; i <= 5; i++)
+        g2d.drawLine(3*X/8 - (X/50), (9+2*i)*Y/21, X-(X/50), (9+2*i)*Y/21);
+
+        g2d.setColor(Color.BLACK);
         g2d.drawImage(aircraftcarrier, 3*X/8, 3*Y/7, null);
         g2d.drawImage(battleship, 4*X/8, 3*Y/7, null);
         g2d.drawImage(destroyer, 5*X/8, 3*Y/7, null);
-        g2d.drawImage(submarine, 6*X/8, 3*Y/7, null);
-        g2d.drawImage(patrol, 7*X/8, 3*Y/7, null);
+        g2d.scale(1,6/7.);
+        g2d.drawImage(submarine, 6*X/8, Y/2, null);
+        g2d.scale(1,7/6.);
+        g2d.scale(1,3/2.);
+        g2d.drawImage(patrol, 7*X/8, 6*Y/21, null);
+        g2d.scale(1, 2/3.);
+//        g2d.scale(3/2.,3/2.);
 
         g2d.setStroke(stroke);
         g2d.drawRect(X/100,Y/100,X-(2*X/100),Y-(2*Y/100));
         g2d.setStroke(stroke2);
-        g2d.setFont(new Font("Times New Roman", Font.BOLD, (X*Y/X+Y)/40));
         g2d.scale(X/400., 3*Y/400.);//cahnge to 200. for GUIv1
         g2d.drawString(rowOneText, 10, 40);//change to 10 for GUIv1
         g2d.drawString(rowTwoText, 10, 40 + ((X*Y)/(X+Y))/10);
@@ -84,8 +106,16 @@ public class ControlPanel extends JComponent {
             submarine = ImageIO.read(new File("src\\Visuals\\Images\\Submarine.png"));
             patrol = ImageIO.read(new File("src\\Visuals\\Images\\Patrol.png"));
 
+            font = Font.createFont(Font.TRUETYPE_FONT, new File("src\\Visuals\\Font\\digital-7.ttf"));
+            font = font.deriveFont(Font.BOLD,28);
+            GraphicsEnvironment ge;
+            ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+            ge.registerFont(font);
+
         }catch(IOException e) {
             System.out.println("Missing one or more images in ControlPanel setImages");
+            e.printStackTrace();
+        } catch (FontFormatException e) {
             e.printStackTrace();
         }
     }
